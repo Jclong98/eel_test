0cd4bc1521932a7d105184320ca19599fa7048ee
Jacob <jclong98@gmail.com>
2019-12-12 00:02:42 -0700

    procedural dropdowns and scrolling

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 import eel
+import tkinter.filedialog as fd
 
 eel.init('web')
 
 @eel.expose
 def askdirectory():
-    print("called")
     fd.Tk().withdraw()
     filepath = fd.askdirectory()
 
     return filepath
 
-eel.start('main.html', size=(1080, 720))
+eel.start('main.html', size=(1200, 720))
